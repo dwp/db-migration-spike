ede5a80111fe8ab3513fd12671b75a154f6eca90
Andrew McGhie <andrew.mcghie@gmail.com>
2017-11-15 22:40:13 +1000

    Added scripts to start haproxy, kafka and mongo docker containers

--- start-haproxy.sh
+++ start-haproxy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-IMAGE_NAME="spotify/kafka"
-CONTAINER_NAME=kafka-server
 
-if [ ! "$(docker images ${IMAGE_NAME} | grep kafka)" ]; then
+IMAGE_NAME="haproxy:1.7"
+CONTAINER_NAME=haproxy-server
+
+if [ ! "$(docker images ${IMAGE_NAME} | grep haproxy)" ]; then
     docker pull ${IMAGE_NAME}
 fi
 
@@ -14,7 +15,7 @@ if [ ! "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
         docker rm ${CONTAINER_NAME}
     fi
     echo "Running ${CONTAINER_NAME}"
-    docker run -d --name ${CONTAINER_NAME} -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=$HOSTIP --env ADVERTISED_PORT=9092 ${IMAGE_NAME}
+    docker run --name ${CONTAINER_NAME} --detach --rm -p 8443:8443 --add-host=docker:${HOSTIP} -v `pwd`/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro ${IMAGE_NAME}
 else
     echo "${CONTAINER_NAME} is already running."
 fi
---
+++ start-mongo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+IMAGE_NAME="mongo:3.2"
+CONTAINER_NAME=mongo-server
+
+if [ ! "$(docker images ${IMAGE_NAME} | grep mongo)" ]; then
+    docker pull ${IMAGE_NAME}
+fi
+
+if [ ! "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=${CONTAINER_NAME})" ]; then
+        echo "Cleaning up ${CONTAINER_NAME}"
+        docker rm ${CONTAINER_NAME}
+    fi
+    echo "Running ${CONTAINER_NAME}"
+    docker run -d --name ${CONTAINER_NAME} ${IMAGE_NAME} --auth
+    # Allow mongo time to start
+    sleep 2
+    echo "Creating admin user"
+    docker exec -it ${CONTAINER_NAME} mongo admin --eval "db.createUser({user:'admin',pwd:'Passw0rd',roles:['userAdminAnyDatabase']})"
+else
+    echo "${CONTAINER_NAME} is already running."
+fi
